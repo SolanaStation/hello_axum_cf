6dfdbd20f0d0f49927f739b356ec7ee56fbbba2f
John Tashiro <solanastation@John.local>
2025-05-10 15:52:42 +0900

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use axum::{routing::get, Router};
 use tower_service::Service;
+
 use worker::*;
 
 fn router() -> Router {
@@ -7,12 +8,16 @@ fn router() -> Router {
 }
 
 #[event(fetch)]
+
 async fn fetch(
     req: HttpRequest,
+
     _env: Env,
+
     _ctx: Context,
 ) -> Result<axum::http::Response<axum::body::Body>> {
     console_error_panic_hook::set_once();
+
     Ok(router().call(req).await?)
 }
 

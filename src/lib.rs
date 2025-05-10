997ce9caaf2bfc31cdcd67696f3710c7170d9598
John Tashiro <solanastation@John.local>
2025-05-10 16:02:18 +0900

    Kat's code

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,28 @@
+use axum::extract::Query;
 use axum::{routing::get, Router};
+use serde::Deserialize;
 use tower_service::Service;
-
 use worker::*;
 
 fn router() -> Router {
     Router::new().route("/", get(root))
 }
 
-#[event(fetch)]
+#[derive(Deserialize)]
+pub struct WalletQuery {
+    wallet_address: String,
+}
 
+#[event(fetch)]
 async fn fetch(
     req: HttpRequest,
-
     _env: Env,
-
     _ctx: Context,
 ) -> Result<axum::http::Response<axum::body::Body>> {
     console_error_panic_hook::set_once();
-
     Ok(router().call(req).await?)
 }
 
-pub async fn root() -> &'static str {
-    "Hello from John's MacBook!"
+pub async fn root(Query(params): Query<WalletQuery>) -> String {
+    format!("Hello Axum! Wallet Address: {}", params.wallet_address)
 }
